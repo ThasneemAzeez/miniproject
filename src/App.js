f19016635e7d5ad8d17c5b5c65d4e0594e22968e
Afsal Azeez <afsal@tripstick.com>
2024-10-19 13:43:02 +0400

    fixed form submission

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 import FrontPage from './components/FrontPage';
 import AdminSignIn from './components/AdminSignIn';
-import { BrowserRouter,  Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Form from './components/Form';
-import Navbar from './components/Navbar';
 import UserFeedback from './components/Feedback';
 import ViewfeedBack from './components/ViewfeedBack';
 
@@ -12,16 +11,14 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
+        <Routes>
 
-      <Route path='/' element={<FrontPage/>}/>
-      <Route path='/admn' element={<AdminSignIn/>}/>
-      <Route path='/Form' element={<Form/>}/>
-      <Route path='/feedback' element={<UserFeedback/>}/>
-      <Route path='/viewfeedback' element={<ViewfeedBack/>}/>
-      </Routes>
-      
-      
+          <Route path='/' element={<FrontPage />} />
+          <Route path='/admn' element={<AdminSignIn />} />
+          <Route path='/Form' element={<Form />} />
+          <Route path='/feedback' element={<UserFeedback />} />
+          <Route path='/viewfeedback' element={<ViewfeedBack />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
